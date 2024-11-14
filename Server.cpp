7eea0caf7f7e5fec2a8f5b7779e2f6f88c3056e1
fv99 <filipvon@protonmail.com>
2024-11-14 16:37:56 +0100

    added basic JOIN and PART

--- Server.cpp
+++ Server.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgray <cgray@student.42.fr>                +#+  +:+       +#+        */
+/*   By: fvonsovs <fvonsovs@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 13:03:09 by cgray             #+#    #+#             */
-/*   Updated: 2024/11/13 16:41:05 by cgray            ###   ########.fr       */
+/*   Updated: 2024/11/14 15:52:22 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -262,3 +262,31 @@ std::string	Server::get_password(){return (_password);}
 std::string	Server::get_start_time(){return (_start_time);}
 int	Server::get_epoll_socket(){return (_epoll_socket);}
 int	Server::get_server_socket(){return (_server_socket);}
+
+// channels
+
+Channel *Server::get_channel(const std::string &name)
+{
+	std::map<std::string, Channel *>::iterator it = _channels.find(name);
+	if (it != _channels.end())
+		return it->second;
+	return NULL;
+}
+
+Channel *Server::create_channel(const std::string &name)
+{
+	Channel *channel = new Channel(name);
+	_channels[name] = channel;
+	return channel;
+}
+
+void Server::remove_channel(const std::string &name)
+{
+	std::map<std::string, Channel *>::iterator it = _channels.find(name);
+
+	if (it != _channels.end())
+	{
+		delete it->second;
+		_channels.erase(it);
+	}
+}
--- ServerCommands.cpp
+++ ServerCommands.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ServerCommands.cpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgray <cgray@student.42.fr>                +#+  +:+       +#+        */
+/*   By: fvonsovs <fvonsovs@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 15:14:51 by cgray             #+#    #+#             */
-/*   Updated: 2024/11/13 16:37:39 by cgray            ###   ########.fr       */
+/*   Updated: 2024/11/14 16:37:27 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,4 +82,87 @@ int Server::MODE(User *user, std::stringstream &command){return (0);}
 int Server::WHO(User *user, std::stringstream &command){return (0);}
 int Server::LIST(User *user, std::stringstream &command){return (0);}
 int Server::PRIVMSG(User *user, std::stringstream &command){return (0);}
-int Server::PART(User *user, std::stringstream &command){return (0);}
+
+
+// https://dd.ircdocs.horse/refs/commands/join
+int Server::JOIN(User *user, std::stringstream &command)
+{
+	std::string name;
+	command >> name;
+
+	if (name.empty())
+	{
+		// ERR_NEEDMOREPARAMS
+		reply(user, "", "461", "JOIN", ":Not enough parameters");
+		return 1;
+	}
+
+	Channel *channel = get_channel(name);
+	if (!channel)
+	{
+		// create if doesnt exist
+		channel = create_channel(name);
+		channel->add_operator(user);
+		
+		// TODO: set default modes here
+	}
+
+	// TODO: add a check for invite-only
+
+	if (channel->is_member(user))
+		return 0;
+
+	channel->add_member(user);
+	user->join_channel(channel);
+
+	// TODO: send join message to the user and members of channel
+
+	// send channel topic
+	if (!channel->get_topic().empty())
+		reply(user, "", "332", name, ":" + channel->get_topic()); // RPL_TOPIC
+	else
+		reply(user, "", "331", name, ":No topic set"); // RPL_NOTOPIC
+
+	// send users in channel
+	std::string members;
+	for (std::set<User *>::iterator it = channel->get_members().begin(); it != channel->get_members().end(); ++it)
+		members = members + (*it)->get_nick() + " ";
+	
+	reply (user, "", "353", "= " + name, members); // RPL_NAMREPLY
+	reply(user, "", "366", name, ":End of /NAMES list"); // RPL_ENDOFNAMES
+	return 0;
+}
+
+// https://dd.ircdocs.horse/refs/commands/part
+int Server::PART(User *user, std::stringstream &command)
+{
+	std::string name;
+	command >> name;
+
+	if (name.empty())
+	{
+		// ERR_NEEDMOREPARAMS
+		reply(user, "", "461", "PART", ":Not enough parameters");
+		return 1;
+	}
+
+	Channel *channel = get_channel(name);
+	if (!channel)
+	{
+		reply(user, "", "403", name, ":No such channel"); // ERR_NOSUCHCHANNEL
+		return 1;
+	}
+	if (!channel->is_member(user))
+	{
+		reply(user, "", "442", name, ":You're not on that channel"); // ERR_NOTONCHANNEL
+		return 1;
+	}
+
+	channel->remove_member(user);
+	user->leave_channel(channel);
+
+	// TODO send PART message to users in channel
+
+
+	return (0);
+}
